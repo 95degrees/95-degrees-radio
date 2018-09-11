fe216a9e85d3e4801237dfbc81f9a929370f0f09
voidinvoid <36088240+voidinvoid@users.noreply.github.com>
2018-09-12 00:12:08 +0100

    added !reload parameters

--- src/main/java/me/voidinvoid/commands/ReloadCommand.java
+++ src/main/java/me/voidinvoid/commands/ReloadCommand.java
@@ -6,15 +6,39 @@ import me.voidinvoid.utils.ChannelScope;
 public class ReloadCommand extends Command {
 
     public ReloadCommand() {
-        super("reload", "Reloads playlists and tasks", null, ChannelScope.DJ_CHAT);
+        super("reload", "Reloads playlists, tasks and adverts", "[playlists|tasks|adverts]", ChannelScope.DJ_CHAT);
     }
 
     @Override
     public void invoke(CommandData data) {
-        Radio.instance.startTaskManager();
+        String[] args = data.getArgs();
+
+        if (args.length > 0) {
+            if (args[0].equalsIgnoreCase("playlists")) {
+                Radio.instance.getOrchestrator().loadPlaylists();
+                data.success("Reloaded playlists");
+
+            } else if (args[0].equalsIgnoreCase("tasks")) {
+                Radio.instance.startTaskManager();
+                data.success("Reloaded tasks");
+
+            } else if (args[0].equalsIgnoreCase("adverts")) {
+                if (Radio.instance.getAdvertisementManager() != null) {
+                    Radio.instance.getAdvertisementManager().reload();
+                    data.success("Reloaded adverts");
+                } else {
+                    data.error("Adverts are not enabled");
+                }
+            } else {
+                data.error("Unknown parameter. Use `playlists`, `tasks`, or `adverts`");
+                return;
+            }
+        }
+
         Radio.instance.getOrchestrator().loadPlaylists();
+        Radio.instance.startTaskManager();
         if (Radio.instance.getAdvertisementManager() != null) Radio.instance.getAdvertisementManager().reload();
 
-        data.getTextChannel().sendMessage("Reloaded playlists, tasks and adverts").queue();
+        data.success("Reloaded playlists, tasks and adverts");
     }
 }
